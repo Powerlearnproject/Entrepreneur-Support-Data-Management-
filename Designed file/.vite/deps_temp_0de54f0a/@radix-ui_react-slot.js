7b77a668678ee4566ccbbc0135b11bc26f5d189f
Tech girl <wambomiriam6@gmail.com>
2025-08-05 12:35:28 +0300

    Organized the files

---
+++ Designed file/.vite/deps_temp_0de54f0a/@radix-ui_react-slot.js
@@ -0,0 +1,16 @@
+import {
+  Slot,
+  Slottable,
+  createSlot,
+  createSlottable
+} from "./chunk-G6JNGMMY.js";
+import "./chunk-ENLNQQLU.js";
+import "./chunk-EWTE5DHJ.js";
+export {
+  Slot as Root,
+  Slot,
+  Slottable,
+  createSlot,
+  createSlottable
+};
+//# sourceMappingURL=@radix-ui_react-slot.js.map

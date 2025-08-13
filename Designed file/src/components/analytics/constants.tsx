c0ea530df3e377fbef52224c7209ab4bf7a42d28
marycathline <122857396+marycathline@users.noreply.github.com>
2025-08-13 14:31:10 +0300

    Delete Designed file directory

--- Designed file/src/components/analytics/constants.tsx
+++
@@ -1,7 +0,0 @@
-export const CHART_COLORS = ['#14b8a6', '#8b5cf6', '#06b6d4', '#84cc16', '#f59e0b'];
-
-export const COUNTRIES = ['kenya', 'uganda', 'rwanda', 'ethiopia', 'tanzania'];
-
-export const VALUE_CHAINS = ['audiovisual', 'fashion', 'performing_arts', 'gaming', 'music', 'visual_arts', 'crafts'];
-
-export const LOAN_TYPES = ['term_loan', 'working_capital', 'equipment_financing', 'trade_finance', 'invoice_financing'];
